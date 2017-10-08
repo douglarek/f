8a1d9fe4735b10d9bcf35efe22a62937861b12bd
Lingchao Xin <douglarek@gmail.com>
2017-10-08 23:07:29 +0800

    Update go tools

--- functions/go_tools.fish
+++ functions/go_tools.fish
@@ -9,9 +9,20 @@ function go_tools -d "install or update go tools"
   for i in "josharian/impl" "nishanths/license" "nsf/gocode" "rogpeppe/godef" "golang/lint/golint" "golang/dep/cmd/dep" "nishanths/go-hgconfig"
     go get -u -x github.com/$i
   end
+
   for i in "cmd/gorename" "cmd/goimports" "cmd/guru"
     go get -u -x golang.org/x/tools/$i
   end
+
   go get -u -x honnef.co/go/tools/...
+
+  go get -u -x github.com/zmb3/gogetdoc
+  go get -u -x github.com/ramya-rao-a/go-outline
+  go get -u -x sourcegraph.com/sqs/goreturns
+  go get -u -x github.com/uudashr/gopkgs/cmd/gopkgs
+  go get -u -x github.com/acroca/go-symbols
+  go get -u -x github.com/cweill/gotests/...
+  go get -u -x golang.org/x/tools/cmd/godoc
+  go get -u -x github.com/fatih/gomodifytags
 end
 
