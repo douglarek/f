a684f99129cbfc905521605885b6db94c3c28187
Lingchao Xin <douglarek@gmail.com>
2018-01-11 23:24:02 +0800

    go tools cleanups

--- functions/go_tools.fish
+++ functions/go_tools.fish
@@ -6,23 +6,24 @@
 #
 
 function go_tools -d "install or update go tools"
-  for i in "josharian/impl" "nishanths/license" "nsf/gocode" "rogpeppe/godef" "golang/lint/golint" "golang/dep/cmd/dep" "nishanths/go-hgconfig"
-    go get -u -x github.com/$i
-  end
-
-  for i in "cmd/gorename" "cmd/goimports" "cmd/guru"
-    go get -u -x golang.org/x/tools/$i
-  end
-
-  go get -u -x honnef.co/go/tools/...
-
-  go get -u -x github.com/zmb3/gogetdoc
-  go get -u -x github.com/ramya-rao-a/go-outline
-  go get -u -x sourcegraph.com/sqs/goreturns
-  go get -u -x github.com/uudashr/gopkgs/cmd/gopkgs
-  go get -u -x github.com/acroca/go-symbols
-  go get -u -x github.com/cweill/gotests/...
-  go get -u -x golang.org/x/tools/cmd/godoc
-  go get -u -x github.com/fatih/gomodifytags
+  go get -x -v golang.org/x/tools/cmd/goimports
+  go get -x -v github.com/golang/dep/cmd/dep
+  go get -x -v honnef.co/go/tools/...
+  go get -x -v github.com/nishanths/license
+  go get -x -v github.com/ramya-rao-a/go-outline
+  go get -x -v github.com/acroca/go-symbols
+  go get -x -v github.com/nsf/gocode
+  go get -x -v github.com/rogpeppe/godef
+  go get -x -v golang.org/x/tools/cmd/godoc
+  go get -x -v github.com/zmb3/gogetdoc
+  go get -x -v github.com/golang/lint/golint
+  go get -x -v github.com/fatih/gomodifytags
+  go get -x -v github.com/uudashr/gopkgs/cmd/gopkgs
+  go get -x -v golang.org/x/tools/cmd/gorename
+  go get -x -v sourcegraph.com/sqs/goreturns
+  go get -x -v github.com/cweill/gotests/...
+  go get -x -v golang.org/x/tools/cmd/guru
+  go get -x -v github.com/josharian/impl
+  go get -x -v github.com/haya14busa/goplay/cmd/goplay
 end
 
