06d944b24a9c7f3e9340b458d4da003972502a0e
Lingchao Xin <douglarek@gmail.com>
2018-09-03 14:23:08 +0800

    go_tools function supports update flag

--- functions/go_tools.fish
+++ functions/go_tools.fish
@@ -6,26 +6,54 @@
 #
 
 function go_tools -d "install or update go tools"
-  go get -x -v golang.org/x/tools/cmd/goimports
-  go get -x -v honnef.co/go/tools/...
-  go get -x -v github.com/nishanths/license
-  go get -x -v github.com/ramya-rao-a/go-outline
-  go get -x -v github.com/acroca/go-symbols
-  go get -x -v github.com/mdempsky/gocode
-  go get -x -v github.com/rogpeppe/godef
-  go get -x -v golang.org/x/tools/cmd/godoc
-  go get -x -v github.com/zmb3/gogetdoc
-  go get -x -v github.com/golang/lint/golint
-  go get -x -v github.com/fatih/gomodifytags
-  go get -x -v github.com/uudashr/gopkgs/cmd/gopkgs
-  go get -x -v golang.org/x/tools/cmd/gorename
-  go get -x -v sourcegraph.com/sqs/goreturns
-  go get -x -v github.com/cweill/gotests/...
-  go get -x -v golang.org/x/tools/cmd/guru
-  go get -x -v github.com/josharian/impl
-  go get -x -v github.com/haya14busa/goplay/cmd/goplay
-  go get -x -v github.com/davidrjenni/reftools/cmd/fillstruct
-  go get -x -v github.com/sourcegraph/go-langserver
-  go get -x -v github.com/alecthomas/gometalinter
+  set -l flags "-x"
+  getopts $argv | while read -l 1 2
+    switch "$1"
+      case _
+
+      case u update
+        set flags $flags "-u"
+
+      case h help
+        printf "Usage: go_tools [--update]\n"
+
+        printf "     -u --update              Update go tools\n"
+        printf "     -h --help                Show this help\n"
+        return
+
+      case \*
+        printf "go_tools: '%s' is not a valid option\n" $1 >& 2
+        go_tools --help >& 2
+        return 1
+    end
+  end
+
+  set -l pkgs                                        \
+    "golang.org/x/tools/cmd/goimports"               \
+    "honnef.co/go/tools/..."                         \
+    "github.com/nishanths/license"                   \
+    "github.com/ramya-rao-a/go-outline"              \
+    "github.com/acroca/go-symbols"                   \
+    "github.com/mdempsky/gocode"                     \
+    "github.com/rogpeppe/godef"                      \
+    "golang.org/x/tools/cmd/godoc"                   \
+    "github.com/zmb3/gogetdoc"                       \
+    "github.com/golang/lint/golint"                  \
+    "github.com/fatih/gomodifytags"                  \
+    "github.com/uudashr/gopkgs/cmd/gopkgs"           \
+    "golang.org/x/tools/cmd/gorename"                \
+    "sourcegraph.com/sqs/goreturns"                  \
+    "github.com/cweill/gotests/..."                  \
+    "golang.org/x/tools/cmd/guru"                    \
+    "github.com/josharian/impl"                      \
+    "github.com/haya14busa/goplay/cmd/goplay"        \
+    "github.com/davidrjenni/reftools/cmd/fillstruct" \
+    "github.com/sourcegraph/go-langserver"           \
+    "github.com/alecthomas/gometalinter"             \
+    "github.com/derekparker/delve/cmd/dlv"           \
+
+  for p in $pkgs
+    go get $flags $p
+  end
 end
 
