22e85a7454f6aedac168187b8095116f9d5c99a3
xinlingchao <douglarek@gmail.com>
2020-12-29 14:13:02 +0800

    A go version switch func

---
+++ functions/go_switch.fish
@@ -0,0 +1,18 @@
+function go_switch --argument-names 'verzion' # since version in fish shell is a bulit-in var
+    if not test -n "$verzion"
+        echo "Usage: go_switch [version]"
+        return 1
+    end
+
+    if not type -q go{$verzion}
+        echo "Go $verzion doesn't exist, start downloading..."
+        go get golang.org/dl/go{$verzion}; or return 1
+        go{$verzion} download; or return 1
+    end
+
+    set go_bin (command -v "go$verzion")
+    ln -sf $go_bin ~/bin/go; or return 1
+    echo "Switched to Go $verzion"
+    set -gx fish_greeting ''
+    exec fish # reload fish
+end
